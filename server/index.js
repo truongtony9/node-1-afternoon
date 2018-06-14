d3d52b7944c99030db38ef69a8a29833c9cf3209
truongtony9 <truong99t@gmail.com>
2018-06-13 19:58:21 -0500

    update

--- server/index.js
+++ server/index.js
@@ -5,12 +5,13 @@ const mc = require(__dirname + "/controllers/messages_controller");
 const app = express();
 
 app.use(bodyParser.json());
+app.use(express.static(__dirname + "/../public/build"));
 
-const messagesBaseUrl = "/api/messages";
-app.post(messagesBaseUrl, mc.create);
-app.get(messagesBaseUrl, mc.read);
-app.put(`${messagesBaseUrl}/:id`, mc.update);
-app.delete(`${messagesBaseUrl}/:id`, mc.delete);
+const baseURL = "/api/messages";
+app.post(baseURL, mc.create);
+app.get(baseURL, mc.read);
+app.put(`${baseURL}/:id`, mc.update);
+app.delete(`${baseURL}/:id`, mc.delete);
 
 const port = 3000;
 app.listen(port, () => {
